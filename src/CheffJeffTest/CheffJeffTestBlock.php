c73d0ba01018072541d236521f01be3b74e30886
CheffJeff <1999jeffreynijkamp@gmail.com>
2025-03-24 09:40:37 +0100

    init block

---
+++ src/CheffJeffTest/CheffJeffTestBlock.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+namespace Cheffjeff\LyfterComposerBlock;
+
+
+use Lyfter\Blocks\BladeBlock;
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+
+class LyfterComposerBlock extends BladeBlock
+{
+   public ?string $icon = 'fa-rocket-launch';
+
+
+   public function __construct()
+   {
+       parent::__construct(
+           __('LyfterComposerBlock', 'lyfter'),
+           __('Dit is een test block...', 'lyfter')
+       );
+   }
+
+
+   public function getConfig(): ?FieldsBuilder
+   {
+       $fields = $this->getFieldsBuilder();
+       $fields->addText('title');
+       $fields->addWysiwyg('content');
+
+
+       return $fields;
+   }
+
+
+   // This is how you customize a param value
+   protected function setTitle(?string $title): string
+   {
+       return $title ?: 'Hello Test block!';
+   }
+
+
+   protected function additionalParams(array $params): array
+   {
+       return [];
+   }
+}
